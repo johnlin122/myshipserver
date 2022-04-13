477e265a19461964b0f1f54375697fda6fb4e080
johnlin122 <103436019+johnlin122@users.noreply.github.com>
2022-04-13 09:26:04 +0800

    springboot框架

---
+++ myshipserver/src/main/java/com/ljy/myshipserver/MyshipserverApplication.java
@@ -0,0 +1,13 @@
+package com.ljy.myshipserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyshipserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyshipserverApplication.class, args);
+    }
+
+}
---
+++ myshipserver/src/test/java/com/ljy/myshipserver/MyshipserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ljy.myshipserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyshipserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
